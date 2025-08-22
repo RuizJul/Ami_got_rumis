848e1346988807647e6c2164ee32e45256fea1ab
RuizJul <137111495+RuizJul@users.noreply.github.com>
2025-08-22 19:24:12 +0000

    commit4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sheetURL = 'https:/https://docs.google.com/spreadsheets/d/e/2PACX-1vTOtxu5Fg8w-RqqI_9v0wDEWl3KHWEU48cRs4KWHWtO06NqGluY-eko3nyyYSTzfSuFXxjT5EPCu-Oc/pub?gid=0&single=true&output=csvdocs.google.com/spreadsheets/d/1S6TSRPI93d6MjCbs8Kde3um0UhIJ0YJlA61ZZTY3cvY/edit?usp=sharing'; // reemplaza con tu link de publicación de Google Sheets
+const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTOtxu5Fg8w-RqqI_9v0wDEWl3KHWEU48cRs4KWHWtO06NqGluY-eko3nyyYSTzfSuFXxjT5EPCu-Oc/pub?gid=0&single=true&output=csv'; // reemplaza con tu link de publicación de Google Sheets
 
 fetch(sheetURL)
   .then(response => response.text())
